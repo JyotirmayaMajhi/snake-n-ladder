518afc1a1530973a051d20c3fd3cb7e09c6e9495
unknown <jyotirmayamajhi1998@gmail.com>
2021-11-01 08:29:23 +0530

    Game played with single player at start position 0

---
+++ src/com/bridgelabz/demo/snakeladdergame_uc1.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.demo;
+import java.util.Scanner;
+public class snakeladdergame_uc1 {
+	public static final int player=0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Start the game=" +player);
+		
+		
+
+	}
+
+}
